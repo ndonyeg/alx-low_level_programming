32b9c3b2d82139ab917b9dd7febf5da3d06dacc1
ndonyeg <jeffndonye@gmail.com>
2023-03-15 02:42:32 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+		
+#include "main.h"
+
+int main(int argc __attribute__((unused)), char *argv[])
+
+{
+
+	printf("%s\n", *argv);
+
+
+
+	return (0);
+
+}
+
---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+		
+#include "main.h"
+int _putchar(char c)
+		
+{
+		
+return (write(1, &c, 1));
+		
+}
+
+
